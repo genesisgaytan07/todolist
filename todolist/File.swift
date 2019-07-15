170c9b9d4142000908479088903a70992fbf6b32
Apple <apple@Apples-MacBook-Pro-19.local>
2019-07-15 11:54:08 -0500

    iteration0

---
+++ todolist/File.swift
@@ -0,0 +1,17 @@
+//
+//  File.swift
+//  todolist
+//
+//  Created by Apple on 7/15/19.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+import UIKit
+class ToDo {
+    var name = ""
+    
+    var important = false
+    
+}
+
+
